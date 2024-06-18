fa2409c0d10f9b0dcefbd1b3317ca42bf3d14343
Martin Matovu <matovius@proton.me>
2024-06-18 11:19:34 +0300

    Overhaul layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,29 @@
 <script lang="ts">
-  import '@fontsource/poppins/400.css';
-  import '@fontsource/poppins/500.css';
-  import '@fontsource/poppins/600.css';
-  import '@fontsource/poppins/800.css';
+  import '@fontsource-variable/recursive';
   import '../app.css';
+	import Header from '$lib/components/Header.svelte';
+  import { AllMoments } from './store';
+
+  function clearMoments() {
+    AllMoments.set([]);
+  }
 </script>
 
-<slot />
+<div class="app">
+  <Header on:clearmoments={clearMoments} />
+  <slot />
+</div>
+
+<style>
+  .app {
+    width: 100dvw;
+    max-width: 100dvw;
+    height: 100dvh;
+    max-height: 100dvh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+  }
+</style>
