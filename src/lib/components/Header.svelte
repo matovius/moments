53bc5fba98f5ff5c9d0ee1858c2226d2d33a8346
Martin Matovu <matovius@proton.me>
2024-06-18 11:28:49 +0300

    Populate header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
   import Logo from "$lib/components/Logo.svelte";
+	import { DropdownMenu } from "bits-ui";
+	import { EllipsisVerticalIcon } from "lucide-svelte";
+	import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher<any>();
+
+  function clearMoments() {
+    dispatch("clearmoments");
+  }
 </script>
 
 <header class="header">
@@ -9,32 +18,45 @@
         <Logo />
       </div>
     </div>
+
     <div class="corner rightCorner">
-      <a href="/demo" class="button solid cta">
-        <span>Open demo</span>
-      </a>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger class="button ghost account" style="--pad-y: 12px" title="More options">
+          <EllipsisVerticalIcon />
+          <span class="sr-only">More options</span>
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content class="dropdown more-options" align="end">
+          <DropdownMenu.Group>
+            <DropdownMenu.Item class="button ghost clear-moments">
+              <span>About</span>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item class="button ghost clear-moments">
+              <span>Export</span>
+            </DropdownMenu.Item>
+          </DropdownMenu.Group>
+
+          <DropdownMenu.Separator class="separator" />
+
+          <DropdownMenu.Group>
+            <DropdownMenu.Item class="button ghost clear-moments" on:click={clearMoments}>
+              <span>Clear Moments</span>
+            </DropdownMenu.Item>
+          </DropdownMenu.Group>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
     </div>
   </div>
 </header>
 
 <style>
   .header {
-    color-scheme: light;
     isolation: isolate;
     width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     padding-inline: 12px;
-    background: hsl(var(--clr-white));
-    position: sticky;
-    top: 0;
-    z-index: 1000;
-
-    @media (prefers-color-scheme: dark) {
-      color-scheme: dark;
-      background: hsl(var(--clr-neutral-950));
-    }
 
     & > .container {
       width: 100%;
@@ -68,4 +90,36 @@
     align-items: center;
     gap: 12px;
   }
+
+  :global(.dropdown) {
+    border-radius: 24px;
+  }
+
+  :global(.dropdown .separator) {
+    width: 100%;
+    height: 1px;
+    background: hsl(var(--clr-neutral-200));
+    margin-block: 8px;
+
+    @media (prefers-color-scheme: dark) {
+      background: hsl(var(--clr-neutral-800));
+    }
+  }
+
+  :global(.dropdown.more-options) {
+    color-scheme: light;
+    color: hsl(var(--clr-neutral-950));
+    width: 240px;
+    padding: 8px;
+    border: 1px solid;
+    border-color: hsl(var(--clr-neutral-200));
+    background: hsl(var(--clr-white));
+
+    @media (prefers-color-scheme: dark) {
+      color-scheme: dark;
+      color: hsl(var(--clr-neutral-50));
+      border-color: hsl(var(--clr-neutral-800));
+      background: hsl(var(--clr-neutral-950));
+    }
+}
 </style>
