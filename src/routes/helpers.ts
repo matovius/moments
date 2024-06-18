7409171332d85e729bdef89aff6ac793c4c3ddcf
Martin Matovu <matovius@proton.me>
2024-06-18 11:37:51 +0300

    Add day and month arrays

---
+++ src/routes/helpers.ts
@@ -0,0 +1,24 @@
+export const Days: string[] = [
+	'Sunday',
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday'
+];
+
+export const Months: string[] = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December'
+];
