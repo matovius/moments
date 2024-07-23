0d46dc500942583ffea0a0a3656cad2ecb90c32b
Martin Matovu <matovius@proton.me>
2024-07-23 18:07:59 +0300

    Add layout

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,13 @@
+<div class="app">
+  <slot />
+</div>
+
+<style>
+  .app {
+    width: 100dvw;
+    max-width: 100dvw;
+    height: 100dvh;
+    max-height: 100dvh;
+    isolation: isolate;
+  }
+</style>
