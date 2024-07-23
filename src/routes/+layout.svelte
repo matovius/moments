0f26691eb7c498da17bcd9f61d30e597e04d99f7
Martin Matovu <matovius@proton.me>
2024-07-23 14:01:12 +0300

    Add font imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,8 @@
 <script lang="ts">
   import '@fontsource-variable/recursive';
+  import '@fontsource-variable/roboto-flex';
+  import '@fontsource-variable/work-sans';
   import '../app.css';
-	import Header from '$lib/components/Header.svelte';
-  import { AllMoments } from './store';
-
-  function clearMoments() {
-    AllMoments.set([]);
-  }
 </script>
 
-<div class="app">
-  <Header on:clearmoments={clearMoments} />
-  <slot />
-</div>
-
-<style>
-  .app {
-    width: 100dvw;
-    max-width: 100dvw;
-    height: 100dvh;
-    max-height: 100dvh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden;
-  }
-</style>
+<slot />
