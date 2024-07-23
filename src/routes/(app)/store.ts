b7ba608dd96563eaeb08597ffbe9e534471f894b
Martin Matovu <matovius@proton.me>
2024-07-23 18:02:53 +0300

    Move stores and types

--- src/routes/(app)/store.ts
+++ src/routes/(app)/store.ts
@@ -1,5 +1,6 @@
 import { writable, type Writable } from 'svelte/store';
 import { type Moment } from './types';
-import { getMoments } from './localMoments';
+import { getMoments } from './demo/localMoments';
 
 export const AllMoments: Writable<Moment[]> = writable<Moment[]>(getMoments());
+export const HeaderHeight: Writable<number> = writable<number>(0);
