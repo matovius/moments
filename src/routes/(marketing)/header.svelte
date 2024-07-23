18e663c1f5d9eef631d5c60bd7cd035d666c6892
Martin Matovu <matovius@proton.me>
2024-07-23 21:19:05 +0300

    Implement mobile nav drawer

--- src/routes/(marketing)/header.svelte
+++ src/routes/(marketing)/header.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import { quadOut } from "svelte/easing";
+	import { fade, fly } from "svelte/transition";
+	import { Button, Dialog } from "bits-ui";
+	import { MenuIcon, XIcon } from "lucide-svelte";
 	import Logo from "$lib/components/Logo.svelte";
-	import { Button } from "bits-ui";
-	import { MenuIcon } from "lucide-svelte";
 
   const NavLinks = [
     {
@@ -45,10 +47,45 @@
         </Button.Root>
       </div>
       <div class="mobile-nav">
-        <Button.Root class="btn btn-ghost">
-          <MenuIcon />
-          <span class="sr-only">Open nav drawer</span>
-        </Button.Root>
+        <Dialog.Root>
+          <Dialog.Trigger class="btn btn-ghost">
+            <MenuIcon />
+            <span class="sr-only">Open nav drawer</span>
+          </Dialog.Trigger>
+
+          <Dialog.Portal class="dialog-portal">
+            <Dialog.Overlay
+              class="dialog-overlay"
+              transition={fade}
+              transitionConfig={{ duration: 100, easing: quadOut }}
+            />
+            <Dialog.Content
+              class="nav-drawer"
+              transition={fly}
+              transitionConfig={{ duration: 100, easing: quadOut, x: "100%", y: 0, opacity: 0 }}
+            >
+              <div class="close-button">
+                <Dialog.Close class="btn btn-inverse">
+                  <XIcon />
+                  <span class="sr-only">Close nav drawer</span>
+                </Dialog.Close>
+              </div>
+              <nav>
+                <ul class="nav-list">
+                  {#each NavLinks as link}
+                    <li><a href={link.url} class="nav-link">{link.label}</a></li>
+                  {/each}
+                  <li>
+                    <Button.Root class="btn btn-default">
+                      <span>Get early access</span>
+                    </Button.Root>
+                  </li>
+                </ul>
+              </nav>
+            </Dialog.Content>
+
+          </Dialog.Portal>
+        </Dialog.Root>
       </div>
     </div>
   </div>
@@ -124,6 +161,42 @@
 
   :global(.cta > .btn) {
     cursor: pointer;
+  }
+  
+  :global(.dialog-portal > .nav-drawer) {
+    width: 100%;
+    max-width: calc(100% - 48px);
+    height: 100dvh;
+    overflow-y: hidden;
+    border-radius: 24px 0 0 24px;
+    background-color: hsl(var(--clr-white));
+    position: fixed;
+    inset-block: 0;
+    right: 0;
+    z-index: 50;
+
+    & .close-button {
+      position: fixed;
+      top: 12px;
+      right: 24px;
+    }
+
+    & > nav {
+      padding: 24px;
+      padding-top: 96px;
+
+      & > .nav-list {
+        list-style: none;
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+      }
+
+    }
+
+    @media screen and (min-width: 400px) {
+      max-width: 360px;
+    }
   }
 </style>
 
