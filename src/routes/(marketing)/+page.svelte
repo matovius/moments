faf6cf4acc91eac35c95de1823202c02fde3e85c
Martin Matovu <matovius@proton.me>
2024-08-19 03:03:27 +0300

    Move landing page into routing group

---
+++ src/routes/(marketing)/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import SiteHeader from "$lib/components/SiteHeader.svelte";
+	import LandingPage from "$lib/components/LandingPage.svelte";
+	import SiteFooter from "$lib/components/SiteFooter.svelte";
+</script>
+
+<svelte:head>
+  <title>Moments - Capture your thoughts</title>
+</svelte:head>
+
+<SiteHeader />
+<LandingPage />
+<SiteFooter />
