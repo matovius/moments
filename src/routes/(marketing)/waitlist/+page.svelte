52325d856588b82dc044fbd3d5fd905b0f1d42c4
Martin Matovu <matovius@proton.me>
2024-08-20 01:35:34 +0300

    add: waitlist page

---
+++ src/routes/(marketing)/waitlist/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import SiteHeader from "$lib/components/SiteHeader.svelte";
+	import SiteFooter from "$lib/components/SiteFooter.svelte";
+  import WaitlistPage from '$lib/components/WaitlistPage.svelte';
+</script>
+
+<svelte:head>
+  <title>Join the waitlist - Moments</title>
+</svelte:head>
+
+<SiteHeader />
+<WaitlistPage />
+<SiteFooter />
