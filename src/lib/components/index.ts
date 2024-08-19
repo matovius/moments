7654b2825c8ed1c14797203bba84bd1066323f15
Martin Matovu <matovius@proton.me>
2024-08-19 03:02:50 +0300

    Remove unused files

--- src/lib/components/index.ts
+++
@@ -1,8 +0,0 @@
-export * as Logo from './Logo.svelte';
-
-export * as LandingPage from './LandingPage.svelte';
-export * as AppHeader from './AppHeader.svelte';
-
-export * as EntryCard from './EntryCard.svelte';
-export * as EntryForm from './EntryForm.svelte';
-export * as EntriesView from './EntriesView.svelte';
