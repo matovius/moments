fa387c392c7861d3faf1554cc52562e6e82ac3fb
Martin Matovu <matovius@proton.me>
2024-08-19 03:24:49 +0300

    Add footer for landing page

---
+++ src/lib/components/SiteFooter.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<footer class="px-24 py-48 border-t border-solid border-neutral-200 dark:border-neutral-800">
+  <div class="text-neutral-600 dark:text-neutral-400 text-center">
+    <small class="small">Copyright {new Date().getFullYear() | 2024} - Moments - All rights reserved.</small>
+  </div>
+</footer>
