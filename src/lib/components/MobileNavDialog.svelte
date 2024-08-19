62fe3fcd3125cc66a124d5981abb657f8efb8df6
Martin Matovu <matovius@proton.me>
2024-08-19 20:20:35 +0300

    change: nav menu position

--- src/lib/components/MobileNavDialog.svelte
+++ src/lib/components/MobileNavDialog.svelte
@@ -38,7 +38,7 @@
   </div>
   {#if open}
     <div class="mobile-nav-dialog" transition:fly={{ duration: 100, easing: quadOut, x: "100%", y: 0, opacity: 0 }}>
-      <nav id="mobile-nav" class="block p-24">
+      <nav id="mobile-nav" class="w-full block p-24">
         <ul class="flex flex-col justify-start items-start gap-24">
           <li>
             <a href="/#" class="nav-link" on:click={() => { open = false; }}>About</a>
@@ -71,6 +71,7 @@
   .mobile-nav-dialog {
     @apply text-neutral-800 dark:text-neutral-200 w-full
     max-w-[calc(100%-24px)] 400:max-w-360 h-[100dvh]
+    flex justify-center items-center
     rounded-l-24 bg-white dark:bg-neutral-900
     fixed top-0 right-0 z-[1];
   }
